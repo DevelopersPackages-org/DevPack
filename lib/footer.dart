50c24ea9967a6e622b12f664ca635e8058774246
Furcht968 <tomu.iwanaga129+github@gmail.com>
2021-01-29 09:31:38 +0000

    add: コンポーネントあたり、改悪の可能性あるかも

---
+++ lib/footer.dart
@@ -0,0 +1,5 @@
+import 'package:angular/angular.dart';
+
+class FooterComponent {
+  // Nothing here yet. All logic is in TodoListComponent.
+}
--- lib/header/header.dart
+++ lib/header/header.dart
@@ -1,6 +1,6 @@
 import 'package:angular/angular.dart';
 import 'logo_area.dart';
-import 'menu/menu_bar.dart';
+import 'menu_bar.dart';
 
 
 @Component(
